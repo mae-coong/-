256eee588e53e8ca4011080ec3c24329af811798
mae-coong <jeormkim@gmail.com>
2020-08-28 19:06:44 +0900

    1강

---
+++ 1강.py
@@ -0,0 +1,16 @@
+# 주석처리
+# 하나만 출력하기
+print("# 하나만 출력하기")
+print(52)
+print(287)
+print()
+# 여러 개 출력하기
+print("#여러 개 출력하기")
+print(52,287,"hello")
+print("느개비""케찹도둑")
+print()
+# 줄바꿈하기
+print("# 줄바꿈 하기")
+print()
+print()
+print("---줄바꿈 끝---")
