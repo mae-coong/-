46e32e3da5bbdb289ea17e8bef036acf2b41fbf0
mae-coong <70145040+mae-coong@users.noreply.github.com>
2020-09-02 00:03:08 +0900

    2-3강

---
+++ 2-3강.py
@@ -0,0 +1,19 @@
+# 변수 선언과 할당
+pi = 3.141592
+r = 10
+# 변수 참조
+print("원주율 =", pi)
+print("반지름 =", r)
+print("원의 둘레 =", 2*pi*r)
+print("원의 넓이 =", pi*r*r)
+
+# 복합 대입 연산자
+# □= 는 □후 대입
+number = 100
+number += 10
+number += 30
+print("number:", number)
+
+string ="자이루"
+string += "!"
+print("string:", string)
